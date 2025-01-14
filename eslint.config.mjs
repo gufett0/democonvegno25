8d25904d8b7ec4e0c51adbc3db7007852acced6d
Marco Esposito <gufett0@tutanota.com>
2025-01-14 16:25:13 +0100

    removed base path

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "react-hooks/exhaustive-deps": "warn",
-      "@typescript-eslint/no-unused-vars": "warn"
-    }
-  }
 ];
 
 export default eslintConfig;
